570d6bc6306536ffe34d05cdc4fd374a01db580d
Eren Sargin <erensargin94@hotmail.com>
2022-04-04 13:18:06 +0300

    HandleOnChange updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,22 @@ import './home.css'
 import Checkbox from '../components/Checkbox';
 
 export default function Home() {
-    const [selections, setSelections] = useState([]);
+    const [selections, setSelections] = useState(['']);
     const [counter, setCounter] = useState(0);
     const [checked, setChecked] = useState(false);
 
     const items = ['1', '2', '3']
 
     const handleOnChange = (e) => {
-        setSelections(e.target.name)
-        console.log(selections)
+        let sel = selections
+        let find = sel.indexOf(e.target.name)
+        if (find > -1) {
+            sel.splice(find, 1)
+        }
+        else {
+            sel.push(e.target.name)
+        }
+        setSelections(sel)
     }
 
     return (
